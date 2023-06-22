dea58163305d15ae951e8e5fff3ea956b6135fa5
cosmopolitan0713 <135200958+cosmopolitan0713@users.noreply.github.com>
2023-06-22 11:27:51 +0900

    タスク⑤「 出品者の商品購入ページ遷移の制御」

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :select_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :redirect_to_show, only: [:edit, :update, :destroy]
-
+  
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -65,4 +65,5 @@ class ItemsController < ApplicationController
   def redirect_to_show
     return redirect_to root_path if current_user.id != @item.user.id
   end
+
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: [:new]
+  before_action :redirect_if_seller, only: [:index,:new, :create]
   
   def index
     @item = Item.find(params[:item_id])
@@ -24,5 +25,12 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture, :city, :addresses, :building, :phone_number).merge(item_id: params[:item_id])
+  end
+
+  def redirect_if_seller
+    @item = Item.find(params[:item_id])
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path, alert: "You cannot purchase your own item."
+    end
   end
 end
